ebc52281101abb2118942d9057968e8bb811a801
programminghoch10 <hoch10@kabelbw.de>
2019-09-05 17:05:32 +0200

    expermiental alpha stage watchface implementation
    i tried myself to death while figuring out how config activites work and why certain fonts werent loaded so here is a basic implementation of the clock which shows date and time with 60 minutes overhang and font magneto.

---
+++ app/src/main/java/com/JJ/hangoverclock/WatchFaceConfig.java
@@ -0,0 +1,14 @@
+package com.JJ.hangoverclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WatchFaceConfig extends Activity {
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_face_config);
+        
+    }
+}
