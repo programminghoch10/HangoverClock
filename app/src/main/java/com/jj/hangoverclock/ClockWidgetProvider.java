964ff4ab3bb0de36c44783892d21819bb8485bb9
programminghoch10 <hoch10@kabelbw.de>
2019-09-08 20:01:18 +0200

    Migrate to AndroidX

--- app/src/main/java/com/jj/hangoverclock/ClockWidgetProvider.java
+++ app/src/main/java/com/jj/hangoverclock/ClockWidgetProvider.java
@@ -10,7 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
-import android.support.v4.app.AlarmManagerCompat;
+import androidx.core.app.AlarmManagerCompat;
 import android.text.format.DateFormat;
 import android.util.Log;
 import android.widget.RemoteViews;
