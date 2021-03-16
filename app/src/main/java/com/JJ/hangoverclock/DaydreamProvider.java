a3412c579e1c1212235cf6eb559f6d29d8f41744
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-03-16 21:58:42 +0100

    performance and reliablity enhancements in daydream
    now using a thread instead of that wacky unreliable ValueAnimator

--- app/src/main/java/com/JJ/hangoverclock/DaydreamProvider.java
+++ app/src/main/java/com/JJ/hangoverclock/DaydreamProvider.java
@@ -1,11 +1,11 @@
 package com.JJ.hangoverclock;
 
-import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.service.dreams.DreamService;
 import android.text.format.DateFormat;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -15,79 +15,104 @@ import java.util.Calendar;
 
 @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
 public class DaydreamProvider extends DreamService {
-	
-	SharedPreferences sharedPreferences;
-	ValueAnimator valueAnimator;
-	private long lastupdate;
-	private int duration;
-	ValueAnimator.AnimatorUpdateListener updatelistener = new ValueAnimator.AnimatorUpdateListener() {
-		@Override
-		public void onAnimationUpdate(ValueAnimator animation) {
-			Context context = DaydreamProvider.this;
-			if (animation.getCurrentPlayTime() < lastupdate + duration) return;
-			lastupdate = animation.getCurrentPlayTime();
-			int houroverhang = sharedPreferences.getInt("houroverhang", context.getResources().getInteger(R.integer.daydreamdefaulthouroverhang));
-			int minuteoverhang = sharedPreferences.getInt("minuteoverhang", context.getResources().getInteger(R.integer.daydreamdefaultminuteoverhang));
-			int secondoverhang = sharedPreferences.getInt("secondoverhang", context.getResources().getInteger(R.integer.daydreamdefaultsecondoverhang));
-			int dayoverhang = sharedPreferences.getInt("dayoverhang", context.getResources().getInteger(R.integer.daydreamdefaultdayoverhang));
-			int monthoverhang = sharedPreferences.getInt("monthoverhang", context.getResources().getInteger(R.integer.daydreamdefaultmonthoverhang));
-			boolean twelvehour = sharedPreferences.getBoolean("twelvehours", !DateFormat.is24HourFormat(context));
-			boolean enableseconds = sharedPreferences.getBoolean("enableseconds", context.getResources().getBoolean(R.bool.daydreamdefaultenableseconds));
-			boolean enabledate = sharedPreferences.getBoolean("enabledate", context.getResources().getBoolean(R.bool.daydreamdefaultenabledate));
-			String font = sharedPreferences.getString("font", context.getResources().getString(R.string.defaultfonttext));
-			float fontscale = sharedPreferences.getFloat("fontscale", context.getResources().getInteger(R.integer.daydreamdefaultfontscale));
-			int color = sharedPreferences.getInt("color", context.getResources().getColor(R.color.daydreamdefaultcolor));
-			((ImageView) findViewById(R.id.daydreamimageview)).setImageBitmap(
-					ClockGenerator.generateClock(
-							DaydreamProvider.this, Calendar.getInstance().getTimeInMillis(),
-							secondoverhang, minuteoverhang, houroverhang, dayoverhang, monthoverhang,
-							twelvehour, enableseconds, enabledate, font, color, fontscale
-					)
-			);
-			//Log.d("DaydreamProvider", "onAnimationUpdate: updated bitmap, animation playtime is " + animation.getCurrentPlayTime());
-		}
-	};
-	
-	@Override
-	public void onAttachedToWindow() {
-		super.onAttachedToWindow();
-		setInteractive(false);    // Exit dream upon user touch
-		//setFullscreen(true);	// Hide status bar
-		int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT)
-			flags |= View.SYSTEM_UI_FLAG_IMMERSIVE;
-		getWindow().getDecorView().setSystemUiVisibility(flags | getWindow().getDecorView().getSystemUiVisibility()); //hides nav bar
-		//TODO: prevent status bar to go to light mode
-		setScreenBright(false);    // wether to set screen brightness to 100%
-		setContentView(R.layout.daydream);    // Set the dream layout
-		sharedPreferences = getSharedPreferences(DaydreamProvider.this.getResources().getString(R.string.daydreampreferencesfilename), MODE_PRIVATE);
-	}
-	
-	@Override
-	public void onDreamingStarted() {
-		super.onDreamingStarted();
-		valueAnimator = new ValueAnimator();
-		valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
-		valueAnimator.setRepeatMode(ValueAnimator.RESTART);
-		valueAnimator.setIntValues(1, 2);
-		duration = sharedPreferences.getBoolean("enableseconds", DaydreamProvider.this.getResources().getBoolean(R.bool.daydreamdefaultenableseconds)) ? 1000 : 60000;
-		//valueAnimator.setDuration(duration);
-		lastupdate = -duration;
-		valueAnimator.addUpdateListener(updatelistener);
-		valueAnimator.start();
-	}
-	
-	@Override
-	public void onDreamingStopped() {
-		super.onDreamingStopped();
-		valueAnimator.end();
-	}
-	
-	@Override
-	public void onDetachedFromWindow() {
-		super.onDetachedFromWindow();
-		valueAnimator.end();
-		valueAnimator = null;
-	}
-	
+
+    private int houroverhang;
+    private int minuteoverhang;
+    private int secondoverhang;
+    private int dayoverhang;
+    private int monthoverhang;
+    private boolean twelvehour;
+    private boolean enableseconds;
+    private boolean enabledate;
+    private String font;
+    private float fontscale;
+    private int color;
+    private int updateseconds;
+    private boolean runUpdateThread = false;
+    private static final String TAG = DaydreamProvider.class.getName();
+    private final Thread updateThread = new Thread() {
+        @Override
+        public void run() {
+            super.run();
+            while (runUpdateThread) {
+                try {
+                    updateDisplay();
+                } catch (RuntimeException e) {
+                    Log.i(TAG, "run: got RuntimeException during updateDisplay, ignoring for now");
+                }
+                try {
+                    Thread.sleep(updateseconds * 1000L);
+                } catch (InterruptedException ignored) {
+                }
+            }
+        }
+    };
+
+    private void updateDisplay() {
+        ((ImageView) findViewById(R.id.daydreamimageview)).setImageBitmap(
+                ClockGenerator.generateClock(
+                        DaydreamProvider.this, Calendar.getInstance().getTimeInMillis(),
+                        secondoverhang, minuteoverhang, houroverhang, dayoverhang, monthoverhang,
+                        twelvehour, enableseconds, enabledate, font, color, fontscale
+                )
+        );
+    }
+
+    private void updateConfiguration() {
+        Context context = DaydreamProvider.this;
+        SharedPreferences sharedPreferences = getSharedPreferences(DaydreamProvider.this);
+        houroverhang = sharedPreferences.getInt("houroverhang", context.getResources().getInteger(R.integer.daydreamdefaulthouroverhang));
+        minuteoverhang = sharedPreferences.getInt("minuteoverhang", context.getResources().getInteger(R.integer.daydreamdefaultminuteoverhang));
+        secondoverhang = sharedPreferences.getInt("secondoverhang", context.getResources().getInteger(R.integer.daydreamdefaultsecondoverhang));
+        dayoverhang = sharedPreferences.getInt("dayoverhang", context.getResources().getInteger(R.integer.daydreamdefaultdayoverhang));
+        monthoverhang = sharedPreferences.getInt("monthoverhang", context.getResources().getInteger(R.integer.daydreamdefaultmonthoverhang));
+        twelvehour = sharedPreferences.getBoolean("twelvehour", !DateFormat.is24HourFormat(context));
+        enableseconds = sharedPreferences.getBoolean("enableseconds", context.getResources().getBoolean(R.bool.daydreamdefaultenableseconds));
+        enabledate = sharedPreferences.getBoolean("enabledate", context.getResources().getBoolean(R.bool.daydreamdefaultenabledate));
+        font = sharedPreferences.getString("font", context.getResources().getString(R.string.defaultfonttext));
+        fontscale = sharedPreferences.getFloat("fontscale", context.getResources().getInteger(R.integer.daydreamdefaultfontscale));
+        color = sharedPreferences.getInt("color", context.getResources().getColor(R.color.daydreamdefaultcolor));
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        setInteractive(false);    // Exit dream upon user touch
+        //setFullscreen(true);	// Hide status bar
+        int flags = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT)
+            flags |= View.SYSTEM_UI_FLAG_IMMERSIVE;
+        getWindow().getDecorView().setSystemUiVisibility(flags | getWindow().getDecorView().getSystemUiVisibility()); //hides nav bar
+        //TODO: prevent status bar to go to light mode
+        setScreenBright(false);    // wether to set screen brightness to 100%
+        setContentView(R.layout.daydream);    // Set the dream layout
+    }
+
+    private SharedPreferences getSharedPreferences(Context context) {
+        return getSharedPreferences(context.getResources().getString(R.string.daydreampreferencesfilename), MODE_PRIVATE);
+    }
+
+    @Override
+    public void onDreamingStarted() {
+        super.onDreamingStarted();
+        SharedPreferences sharedPreferences = getSharedPreferences(DaydreamProvider.this);
+        updateseconds = sharedPreferences.getBoolean("enableseconds", DaydreamProvider.this.getResources().getBoolean(R.bool.daydreamdefaultenableseconds))
+                ? 1 : 60;
+        updateConfiguration();
+        runUpdateThread = true;
+        updateThread.start();
+    }
+
+    @Override
+    public void onDreamingStopped() {
+        super.onDreamingStopped();
+        runUpdateThread = false;
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        runUpdateThread = false;
+    }
+
 }
