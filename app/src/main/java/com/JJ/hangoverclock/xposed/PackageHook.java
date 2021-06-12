e1bbb15a090a83e695c117e282a9b574d7d7c57e
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-12 21:07:46 +0200

    added xposed module stuff

---
+++ app/src/main/java/com/JJ/hangoverclock/xposed/PackageHook.java
@@ -0,0 +1,19 @@
+package com.JJ.hangoverclock.xposed;
+
+import android.graphics.Color;
+import android.widget.TextView;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class PackageHook implements IXposedHookLoadPackage {
+	@Override
+	public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		if (!lpparam.packageName.equals("com.android.systemui")) return;
+		XposedBridge.log("Hangoverclock: Hooking SystemUI Clock");
+	}
+}
