5bf6b5ab01dc417ec9447c0fcf29a6b88ce76de4
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-13 23:58:17 +0200

    added settings activity

---
+++ app/src/main/java/com/JJ/hangoverclock/SettingsActivity.java
@@ -0,0 +1,20 @@
+package com.JJ.hangoverclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import java.io.IOException;
+
+public class SettingsActivity extends Activity {
+	private static final String TAG = SettingsActivity.class.getName();
+	@Override
+	protected void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+		});
+	}
+}
