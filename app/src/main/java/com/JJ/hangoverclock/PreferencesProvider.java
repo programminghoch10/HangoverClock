ee34bbf0011614ef70772299b2e127241f44e3ba
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2021-06-14 03:55:41 +0200

    added security to preferences provider
    only systemui and ourselves are allowed to query the preferences provider

--- app/src/main/java/com/JJ/hangoverclock/PreferencesProvider.java
+++ app/src/main/java/com/JJ/hangoverclock/PreferencesProvider.java
@@ -1,11 +1,24 @@
 package com.JJ.hangoverclock;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import com.crossbowffs.remotepreferences.RemotePreferenceProvider;
 
+@RequiresApi(api = Build.VERSION_CODES.Q)
 public class PreferencesProvider extends RemotePreferenceProvider {
 	private static final String[] files = {"statusbar", "lockscreen"};
 	
 	public PreferencesProvider() {
 		super("com.JJ.hangoverclock.PreferencesProvider", files);
+	}
+	
+	@Override
+	protected boolean checkAccess(String prefFileName, String prefKey, boolean write) {
+		if (write) return false;
+		if ("com.android.systemui".equals(getCallingPackage())) return true;
+		if ("com.JJ.hangoverclock".equals(getCallingPackage())) return true;
+		return false;
 	}
 }
